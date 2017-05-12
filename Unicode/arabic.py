5bcf31b9059921037f6f315667d20eeca721049c
Madison MacNichol <madimac711@gmail.com>
2017-05-11 21:00:10 -0700

    playing with unicode

---
+++ Unicode/arabic.py
@@ -0,0 +1,19 @@
+
+
+s = u'\u0627\u0644\u0639\u062a\u0629'
+
+encoded = s.encode('utf-8')
+print(encoded)
+
+v = u'\u6211\u559c\u6b22\u4f60\u000a'
+
+print(v)
+
+
+l = u'\u0627\u0644\u0623\u0631\u062F\u0646 '
+
+print(l)
+
+r = u'\u6211\u89c9\u5f97\u4f60\u5f88\u68d2\u0021\u0021'
+
+print(r)
